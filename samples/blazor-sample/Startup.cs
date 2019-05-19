c936532bc926f09249e981be350d765cab980b65
Tom Woodforde <tom.woodforde@redox-software.co.uk>
2019-05-19 11:24:59 +0100

    Made the changes needed to get the modal working in Core 3 Preview 5.

--- samples/blazor-sample/Startup.cs
+++ samples/blazor-sample/Startup.cs
@@ -1,6 +1,8 @@
 using BlazorModal;
-using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace blazor_sample
 {
@@ -8,12 +10,34 @@ namespace blazor_sample
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages(); 
+            services.AddServerSideBlazor();
             services.AddBlazorModal();
         }
 
-        public void Configure(IBlazorApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.AddComponent<App>("app");
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
         }
     }
 }
---
+++ src/BlazorModal/Modal.razor.cs
@@ -0,0 +1,49 @@
+﻿// ReSharper disable CheckNamespace
+using BlazorModal.Services;
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorModal
+{
+    /// <summary>
+    /// https://www.telerik.com/blogs/creating-a-reusable-javascript-free-blazor-modal
+    /// </summary>
+    public class ModalBase : ComponentBase, IDisposable
+    {
+        [Inject] ModalService ModalService { get; set; }
+
+        protected bool IsVisible { get; set; }
+        protected string Title { get; set; }
+        protected RenderFragment Content { get; set; }
+
+        protected override void OnInit()
+        {
+            ModalService.OnShow += ShowModal;
+            ModalService.OnClose += CloseModal;
+        }
+
+        public void ShowModal(string title, RenderFragment content)
+        {
+            Title = title;
+            Content = content;
+            IsVisible = true;
+
+            StateHasChanged();
+        }
+
+        public void CloseModal()
+        {
+            IsVisible = false;
+            Title = "";
+            Content = null;
+
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            ModalService.OnShow -= ShowModal;
+            ModalService.OnClose -= CloseModal;
+        }
+    }
+}
